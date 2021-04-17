2542b97f56fdff4a3ac00bff5d7f57e4ab50dd50
Alyssa Concepcion <alyssanconcepcion@gmail.com>
2021-04-17 15:05:42 -0700

    pushing new changes to the js and html

--- assets/java.js
+++ assets/java.js
@@ -1,4 +1,5 @@
 var cityName = document.getElementById("cityName");
+var currentIcon = document.getElementById("current-icon");
 var currentDate = document.getElementById("currentDate");
 var cityTemp = document.getElementById("cityTemp");
 var cityHumid = document.getElementById("cityHumid");
@@ -24,6 +25,8 @@ function getCoordinate() {
     })
     .then(function (data) {
         console.log(data)
+        cityName.textContent = insertCity
+        cityName.setAttribute("class","text-uppercase")
         for (var i=0; i<data.length; i++) {
             localStorage.setItem("cityLat",JSON.stringify(data[i].lat))
             localStorage.setItem("cityLong",JSON.stringify(data[i].lon))
@@ -35,44 +38,71 @@ function getCoordinate() {
 
 
 //pulling from weather API to get info..
-// function getForecast() {
-//     var getLat = JSON.parse(localStorage.getItem("cityLat"));
-//     var getLon = JSON.parse(localStorage.getItem("cityLong"));
-//     // console.log(stringCity)
-//     var requestForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + getLat +"&lon=" + getLon+ "&appid=638e975d4bf76d78330b0c4022872572" ;
-//     fetch(requestForecast)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data)
-//         for (var i=0; i< data.length; i++) {
-//             var createTableRow = document.createElement('tr');
-//             var tableData = document.createElement('td');
-//             var cardInfo = document.createElement('p');
-//             cardInfo.textContent = data[i].list.dt_txt;
+function getForecast() {
 
-//             tableData.appendChild(cardInfo);
-//             createTableRow.appendChild(tableData);
-//             forecastContainer.appendChild(createTableRow);
-//         }
-//     })
-//     }
-
-function getCurrent() {
     var getLat = JSON.parse(localStorage.getItem("cityLat"));
     var getLon = JSON.parse(localStorage.getItem("cityLong"));
-    var requestCurrent = "https://api.openweathermap.org/data/2.5/onecall?lat=" + getLat +"&lon=" + getLon+ "&appid=638e975d4bf76d78330b0c4022872572" ;
-    fetch(requestCurrent)
+    // console.log(stringCity)
+    var requestForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + getLat +"&lon=" + getLon+ "&appid=638e975d4bf76d78330b0c4022872572" ;
+    fetch(requestForecast)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data)
-        for (var i = 0; data.length; i++) {
+            var createTableRow = document.createElement('tr');
+            var tableData = document.createElement('td');
+            var cardInfo = document.createElement('p');
+            cardInfo.textContent = data.
+
+            tableData.appendChild(cardInfo);
+            createTableRow.appendChild(tableData);
+            forecastContainer.appendChild(createTableRow);
+      }
+    )}
+    
+
+function getCurrent() {
+    
+    var getLat = JSON.parse(localStorage.getItem("cityLat"));
+    var getLon = JSON.parse(localStorage.getItem("cityLong"));
+    var requestCurrent = "https://api.openweathermap.org/data/2.5/onecall?lat=" + getLat +"&lon=" + getLon+ "&appid=638e975d4bf76d78330b0c4022872572&units=imperial" ;
+    fetch(requestCurrent)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data) 
+           
+            cityTemp.textContent = data.current.temp + "°F";
+            cityHumid.textContent = data.current.humidity + "%";
+            cityWind.textContent = data.current.wind_speed + " mph";
+            currentIcon.textContent = data.current.weather[0].icon;
+
+            // cityUV.setAttribute = ("class", "text-white");
+            if (data.current.uvi <= 2) {
+                cityUV.textContent = data.current.uvi;
+                cityUV.setAttribute("class", "badge bg-#198754");
+
+            }
+            if (3 <= data.current.uvi <= 5) {
+                cityUV.textContent = data.current.uvi;
+                cityUV.setAttribute("class", "badge bg-warning");
+            }
+            if (6 <= data.current.uvi <=7) {
+                cityUV.textContent = data.current.uvi;
+                cityUV.setAttribute("class","badge bg-#0dcaf0");
+            }
+            if(8 <= data.current.uvi <=10) {
+                cityUV.textContent = data.current.uvi;
+                cityUV.setAttribute("class","badge bg-danger")
+            }
+            if (data.current.uvi > 10) {
+                cityUV.textContent = data.current.uvi;
+                cityUV.setAttribute("class", "badge bg-#6f42c1")
+            }
             
-            
-        }
+        
     })
 }
 
@@ -81,4 +111,5 @@ function getCurrent() {
 searchCity.addEventListener('click',() => {
     getCoordinate();
     getCurrent();
+    getForecast();
 })
